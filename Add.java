a02f60008b6b75bab79acdeb7e1b8cbcd6f83023
Rampatil1305 <ramanagoudsp13@gmail.com>
2025-04-23 15:12:37 +0530

    jai sri ram

--- Add.java
+++ Add.java
@@ -5,8 +5,12 @@ class demo{
         int b=20;
         int sum=a+b;
         int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
         System.out.println("sum is=" +sum);
         System.out.println("sub is=" +sub);
+        System.out.println("mul is=" +mul);
+        System.out.println("div is=" +div);
     }
 
 }
